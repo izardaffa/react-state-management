649eab33886e25721572c5765f92615aa7272c6b
Nizar Daffa <izardev@gmail.com>
2025-08-21 13:19:09 +0700

    add steps-form

---
+++ steps-form/src/App.js
@@ -0,0 +1,67 @@
+import { useState } from "react";
+
+const stepItems = ["Register", "Verification", "Complete Profile"];
+
+function App() {
+  return (
+    <>
+      <Step />
+    </>
+  );
+}
+
+function Step() {
+  const [step, setStep] = useState(1);
+  const [isOpen, setIsOpen] = useState(true);
+
+  function handleNext() {
+    if (step < 3) {
+      setStep((step) => step + 1);
+    }
+  }
+
+  function handlePrev() {
+    if (step > 1) setStep((step) => step - 1);
+  }
+
+  return (
+    <>
+      <button className="close" onClick={() => setIsOpen((isOpen) => !isOpen)}>
+        &times;
+      </button>
+
+      {isOpen && (
+        <div className="steps">
+          <div className="numbers">
+            <div className={step >= 1 ? "active" : ""}>1</div>
+            <div className={step >= 2 ? "active" : ""}>2</div>
+            <div className={step >= 3 ? "active" : ""}>3</div>
+          </div>
+
+          <p className="message">
+            Step {step}: {stepItems[step - 1]}
+          </p>
+
+          <div className="buttons">
+            <button
+              className="previous"
+              onClick={handlePrev}
+              disabled={!(step > 1)}
+            >
+              Prev
+            </button>
+            <button
+              className="next"
+              onClick={handleNext}
+              disabled={!(step < 3)}
+            >
+              Next
+            </button>
+          </div>
+        </div>
+      )}
+    </>
+  );
+}
+
+export default App;
